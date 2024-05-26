ae9ae8c7ba6731e088be9379948c10703b68b7d6
prasad23 <prasadtulshigiri23@gmail.com>
2024-05-26 17:41:01 +0530

    Worked on backend auth validator part

--- backend/router/auth-router.js
+++ backend/router/auth-router.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const authControllers = require("../controllers/auth-controller");
-const signupSchema = require("../validators/auth-validator");
+const {signupSchema, loginSchema} = require("../validators/auth-validator");
 const validate = require("../middlewares/validate-middleware");
 const authMiddleware = require("../middlewares/auth-middleware");
 
@@ -14,11 +14,9 @@ const authMiddleware = require("../middlewares/auth-middleware");
 
 router.route("/").get(authControllers.home);
 
-router
-.route("/register")
-.post( validate(signupSchema), authControllers.register);
+router.route("/register").post( validate(signupSchema), authControllers.register);
 
-router.route("/login").post(authControllers.login);
+router.route("/login").post(validate(loginSchema), authControllers.login);
 
 router.route("/user").get(authMiddleware, authControllers.user);
 
--- backend/validators/auth-validator.js
+++ backend/validators/auth-validator.js
@@ -1,25 +1,12 @@
 const { z } = require("zod");
 
-//Creating an Object Schema
-const signupSchema = z.object({
-    username: z
-    .string({required_error:"Name is Required"})
-    .trim()
-    .min(3, {message:"Name must be atleast 3 characters"})
-    .max(255, {message:"Name Must not be more than 255 Characters"}),
-
+const loginSchema = z.object({
     email: z
     .string({required_error:"Email is Required"})
     .trim()
     .email({message:"Invalid Email Address"})
     .min(3, {message:"Email must be atleast 3 characters"})
     .max(255, {message:"Email Must not be more than 255 Characters"}),
-
-    phone: z
-    .string({required_error:"Phone Number is Required"})
-    .trim()
-    .min(10, {message:"Phone must be atleast 10 characters"})
-    .max(20, {message:"Phone Must not be more than 20 Characters"}),
 
     password: z
     .string({required_error:"Password is Required"})
@@ -28,4 +15,19 @@ const signupSchema = z.object({
     .max(1024, {message:"Password Must not be more than 1024 Characters"}),
 });
 
-module.exports = signupSchema;
+//Creating an Object Schema
+const signupSchema = loginSchema.extend({
+    username: z
+    .string({required_error:"Name is Required"})
+    .trim()
+    .min(3, {message:"Name must be atleast 3 characters"})
+    .max(255, {message:"Name Must not be more than 255 Characters"}),
+
+    phone: z
+    .string({required_error:"Phone Number is Required"})
+    .trim()
+    .min(10, {message:"Phone must be atleast 10 characters"})
+    .max(20, {message:"Phone Must not be more than 20 Characters"}),
+});
+
+module.exports = {signupSchema, loginSchema};
